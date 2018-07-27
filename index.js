5b8bb8d02d1316a8e54d85d7a973d465ccca4952
Amir Safoev <amirsafoev0@gmail.com>
2018-07-27 16:36:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ function removeLastKitten(){
  return kittens.slice(0, kittens.length - 1);
 }
 function removeFirstKitten(){
-  return kittens.splice(1, "Milo");
+  return kittens.splice(0, "Otis");
 }
